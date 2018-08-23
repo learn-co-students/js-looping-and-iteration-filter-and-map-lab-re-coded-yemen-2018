1269bfae5b7b6537efd5a593cb0be83d44c75c90
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-08-23 17:39:45 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here:
+function driversWithRevenueOver(array, string){
+  return array.filter(function(elment){
+    return elment.revenue > string;
+  });
+};
+function driverNamesWithRevenueOver(array, string){
+  return driversWithRevenueOver(array, string).map(function(elment){
+    return elment.name;
+  });
+};
+function exactMatch(array, obj){
+  return array.filter(function(elment){
+    let flag = false;
+    for(const key in obj){
+      flag = elment[key] === obj[key];
+    }
+    return flag;
+  });
+};
+function exactMatchToList (array, obj) {
+  return exactMatch(array, obj).map(function (elment) {
+      return elment.name;
+    });
+}
